61075828910f22f24e948c57d267515e58a4a8c6
Nima Seyf <nimaseyf@gmail.com>
2017-07-05 12:09:56 +0100

    added buttons, fixed font-sizing

--- src/components/TextShadowAssist.js
+++ src/components/TextShadowAssist.js
@@ -10,10 +10,10 @@ constructor(props) {
     color: "#151515",
     font: "",
     fontWeight: "",
-    fontSize: 60,
-    shadowLength: "",
-    shadowColor: "",
-    shadowValues: "",
+    fontSize: 50,
+    shadowLength: 0,
+    shadowColor: "blue",
+    shadowValues: "1px 1px 0px",
     backgroundColor: "#fff",
   }
 }
@@ -36,13 +36,29 @@ boldFont(e) {
   })
 };
 
-generateTextShadow(e) {
+updateShadowLength(e) {
 
+function generateValues(length){
+    const result = [];
+    for(var i=1; i <= length; i++) {
+      result.push(i + "px " + i + "px 0px " + this.state.shadowColor);
+    }
+    return result.join(',')
+  };
+
+
+  this.setState({
+    shadowLength: e.target.value}, () => {
+    this.setState({
+    shadowValues: generateValues(this.state.shadowLength)
+  });
+
+  });
 }
 
 
     render() {
-
+// Styles
 const mainStyle = {
   width: "100%",
   height: "400px",
@@ -68,33 +84,63 @@ color: "grey",
 textAlign: "center"
 }
 
-// need ShadowLength and Shadow Color;
+const numberInputStyle = {
+  fontSize:"40px",
+  width:"90px",
+  textAlign: "center",
+  marginBottom: "5px"
+}
 
+// need ShadowLength and Shadow Color;
+console.log(this.state);
       return (
       <div className="container">
       <h1 style={{fontWeight: "900"}}>Text Shadow Assistant</h1>
 
       <div className="main" style={mainStyle}>
-      <h1 className="word" style={wordStyle}>{this.state.enteredWord}</h1>
+      <h1 className="word"
+      style={wordStyle}>{this.state.enteredWord}</h1>
+
       </div>
 
       <div className="inputs text-center">
+<div className="row">
+      <input value={this.state.enteredWord}
+      style={wordInputStyle}
+      onChange={this.updateWord.bind(this)}/>
+      <button
+      onClick={this.boldFont.bind(this)}>Bold</button>
+</div>
+<div className="row">
+      <div className="col-xs-12 col-lg-3 font-sizing-controls">
+      <h4>Font Size </h4>
+      <input value={this.state.fontSize}
+      style={numberInputStyle}
+      onChange={this.updateFontSize.bind(this)}/>
 
-      <input style={wordInputStyle} value={this.state.enteredWord} onChange={this.updateWord.bind(this)}/>
-          <button onClick={this.boldFont.bind(this)}>Bold</button>
-
-      <div className="font-sizing-controls">
-
-      <input value={this.state.fontSize} style={{fontSize:"40px",width:"90px", textAlign: "center"}} onChange={this.updateFontSize.bind(this)}/>
-
-      <div><button className="btn" style={{margin: "2px"}} onClick={()=> {this.setState({fontSize: this.state.fontSize += 1})}}>+</button>
-      <button className="btn" style={{margin: "2px"}} onClick={()=> {this.setState({fontSize: this.state.fontSize -= 1})}}>-</button>
+      <div>
+      <button className="btn" style={{margin: "2px"}}
+      onClick={()=> {this.setState({fontSize: this.state.fontSize + 1})}}>+</button>
+      <button className="btn" style={{margin: "2px"}}
+      onClick={()=> {this.setState({fontSize: this.state.fontSize - 1})}}>-</button>
       </div>
 
       </div>
-
+      <div className="col-xs-12 col-lg-3">
+      <h4>Shadow Length</h4>
+      <input
+      style={numberInputStyle}
+      value={this.state.shadowLength}
+      onChange={this.updateShadowLength.bind(this)} />
+      <div>
+      <button className="btn" style={{margin: "2px"}}
+      onClick={()=> {this.setState({shadowLength: this.state.shadowLength + 1})}}>+</button>
+      <button className="btn" style={{margin: "2px"}}
+      onClick={()=> {this.setState({shadowLength: this.state.shadowLength - 1})}}>-</button>
       </div>
-
+      </div>
+      </div>
+      </div>
       </div>
 
     )
